57e4158bcbf5b7113ffd6f1ebeaa77487562b2df
fayzulloqodirjonoff <fqodirjonov1@gmail.com>
2025-03-26 15:31:35 +0500

    first commit

--- main.js
+++ main.js
@@ -1,85 +1,83 @@
+class Book {
+  constructor(nomi, muallif, janr) {
+    this.nomi = nomi;
+    this.muallif = muallif;
+    this.janr = janr;
+    this.mavjudligi = true;
+  }
+  getDetails() {
+    return `ismi: ${this.nomi}, muallif: ${this.muallif}, janri: ${this.janr}, mavjudligi: ${this.mavjudligi}`;
+  }
+  markAsBorrowed() {
+    this.mavjudligi = false;
+  }
+  markAsReturned() {
+    this.mavjudligi = true;
+  }
+}
 
-// 1masala
-// class Book {
-//     constructor(nomi, muallif, janr) {
-//       this.nomi = nomi;
-//       this.muallif = muallif;
-//       this.janr = janr;
-//       this.mavjudligi = true;
-//     }
-//     getDetails() {
-//       return `ismi: ${this.nomi}, muallif: ${this.muallif}, janri: ${this.janr}, mavjudligi: ${this.mavjudligi}`;
-//     }
-//     markAsBorrowed() {
-//       this.mavjudligi = false;
-//     }
-//     markAsReturned() {
-//       this.mavjudligi = true;
-//     }
-//   }
-  
-//   class Library {
-//     constructor() {
-//       this.books = [];
-//     }
-//     addBook(book) {
-//       if (!this.books.some((b) => b.nomi === book.nomi)) {
-//         this.books.push(book);
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     }
-//     removeBook(nomi) {
-//       this.books = this.books.filter((book) => book.nomi !== nomi);
-//     }
-//     searchByAuthor(muallif) {
-//       return this.books.filter((book) => book.muallif === muallif);
-//     }
-//     listAvailableBooks() {
-//       return this.books.filter((book) => book.mavjudligi);
-//     }
-//   }
-  
-//   const library = new Library();
-//   const bookListDiv = document.getElementById("bookList");
-//   const genreSearchInput = document.getElementById("genreSearchInput");
-//   const searchButton = document.getElementById("searchButton");
-//   const newTitleInput = document.getElementById("newTitleInput");
-//   const newAuthorInput = document.getElementById("newAuthorInput");
-//   const newGenreInput = document.getElementById("newGenreInput");
-//   const addBookButton = document.getElementById("addBookButton");
-  
-//   function displayBooks(books) {
-//     bookListDiv.innerHTML = "";
-//     books.forEach((book) => {
-//       const bookdiv = document.createElement("div");
-//       bookdiv.textContent = book.getDetails();
-//       bookListDiv.appendChild(bookdiv);
-//     });
-//   }
-  
-//   displayBooks(library.listAvailableBooks());
-  
-//   addBookButton.addEventListener("click", () => {
-//     const nomi = newTitleInput.value;
-//     const muallif = newAuthorInput.value;
-//     const janr = newGenreInput.value;
-  
-//     if (nomi && muallif && janr) {
-//       const newbook = new Book(nomi, muallif, janr);
-//       if (library.addBook(newbook)) {
-//         displayBooks(library.listAvailableBooks());
-//         newTitleInput.value = "";
-//         newAuthorInput.value = "";
-//         newGenreInput.value = "";
-//       } else {
-//         alert("Bu nomli kitob allaqachon mavjud.");
-//       }
-//     } else {
-//       alert("Iltimos, barcha maydonlarni to'ldiring.");
-//     }
-//   });
+class Library {
+  constructor() {
+    this.books = [];
+  }
+  addBook(book) {
+    if (!this.books.some((b) => b.nomi === book.nomi)) {
+      this.books.push(book);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  removeBook(nomi) {
+    this.books = this.books.filter((book) => book.nomi !== nomi);
+  }
+  searchByAuthor(muallif) {
+    return this.books.filter((book) => book.muallif === muallif);
+  }
+  listAvailableBooks() {
+    return this.books.filter((book) => book.mavjudligi);
+  }
+}
+
+const library = new Library();
+const bookListDiv = document.getElementById("bookList");
+const genreSearchInput = document.getElementById("genreSearchInput");
+const searchButton = document.getElementById("searchButton");
+const newTitleInput = document.getElementById("newTitleInput");
+const newAuthorInput = document.getElementById("newAuthorInput");
+const newGenreInput = document.getElementById("newGenreInput");
+const addBookButton = document.getElementById("addBookButton");
+
+function displayBooks(books) {
+  bookListDiv.innerHTML = "";
+  books.forEach((book) => {
+    const bookdiv = document.createElement("div");
+    bookdiv.textContent = book.getDetails();
+    bookListDiv.appendChild(bookdiv);
+  });
+}
+
+displayBooks(library.listAvailableBooks());
+
+addBookButton.addEventListener("click", () => {
+  const nomi = newTitleInput.value;
+  const muallif = newAuthorInput.value;
+  const janr = newGenreInput.value;
+
+  if (nomi && muallif && janr) {
+    const newbook = new Book(nomi, muallif, janr);
+    if (library.addBook(newbook)) {
+      displayBooks(library.listAvailableBooks());
+      newTitleInput.value = "";
+      newAuthorInput.value = "";
+      newGenreInput.value = "";
+    } else {
+      alert("Bu nomli kitob allaqachon mavjud.");
+    }
+  } else {
+    alert("Iltimos, barcha maydonlarni to'ldiring.");
+  }
+});
 // // 2masala
 // class Bus {
 //     constructor(id, route, seats) {
@@ -88,11 +86,11 @@
 //       this.seats = seats;
 //       this.reservedSeats = 0;
 //     }
-  
+
 //     getDetails() {
 //       return `ID: ${this.id}, Yo'nalish: ${this.route}, O'rinlar: ${this.seats}, Band qilingan o'rinlar: ${this.reservedSeats}`;
 //     }
-  
+
 //     reserveSeat() {
 //       if (this.reservedSeats < this.seats) {
 //         this.reservedSeats++;
@@ -100,7 +98,7 @@
 //       }
 //       return false;
 //     }
-  
+
 //     cancelReservation() {
 //       if (this.reservedSeats > 0) {
 //         this.reservedSeats--;
@@ -109,24 +107,24 @@
 //       return false;
 //     }
 //   }
-  
+
 //   class BusSystem {
 //     constructor() {
 //       this.buses = [];
 //     }
-  
+
 //     addBus(bus) {
 //       this.buses.push(bus);
 //     }
-  
+
 //     listBuses() {
 //       return this.buses.map(bus => bus.getDetails());
 //     }
-  
+
 //     searchByRoute(route) {
 //       return this.buses.filter(bus => bus.route === route).map(bus => bus.getDetails());
 //     }
-  
+
 //     reserveSeat(busId) {
 //       const bus = this.buses.find(bus => bus.id === busId);
 //       if (bus) {
@@ -134,7 +132,7 @@
 //       }
 //       return false;
 //     }
-  
+
 //     cancelReservation(busId) {
 //       const bus = this.buses.find(bus => bus.id === busId);
 //       if (bus) {
@@ -143,10 +141,10 @@
 //       return false;
 //     }
 //   }
-  
+
 //   const busSystem = new BusSystem();
 //   const busListDiv = document.getElementById("busList");
-  
+
 //   function displayBuses(buses) {
 //     busListDiv.innerHTML = "";
 //     buses.forEach(bus => {
@@ -155,14 +153,14 @@
 //       busListDiv.appendChild(busDiv);
 //     });
 //   }
-  
+
 //   displayBuses(busSystem.listBuses());
-  
+
 //   document.getElementById("addBusBtn").addEventListener("click", () => {
 //     const id = parseInt(document.getElementById("busId").value);
 //     const route = document.getElementById("route").value;
 //     const seats = parseInt(document.getElementById("seats").value);
-  
+
 //     if (id && route && seats) {
 //       busSystem.addBus(new Bus(id, route, seats));
 //       displayBuses(busSystem.listBuses());
@@ -170,9 +168,8 @@
 //       alert("Iltimos, barcha maydonlarni to'ldiring!");
 //     }
 //   });
-  
+
 //   document.getElementById("searchBtn").addEventListener("click", () => {
 //     const route = document.getElementById("searchRoute").value;
 //     displayBuses(busSystem.searchByRoute(route));
 //   });
-
